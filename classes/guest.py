6bc0c7b7a09a72c3e345f27f3e06cc9f5356d92e
Aodren COMMUN <69194856+Deden-77@users.noreply.github.com>
2024-02-10 17:20:24 +0100

    Add .staff, .organizer, .display_name). Also add parameters to create Guest from nothing.

--- classes/guest.py
+++ classes/guest.py
@@ -9,17 +9,31 @@ class Sex(enum.Enum):
 
 
 class Guest():
-    def __init__(self, first_name: str, last_name: str, birth: datetime.date, sex: Sex, balance: int, adherant_bde: bool) -> None:
-        self.id: str
+    def __init__(
+            self,
+            guest_id: str,
+            first_name: str,
+            last_name: str,
+            birth: datetime.date,
+            # sex: Sex,
+            balance: int,
+            # adherant_bde: bool,
+            staff: int = False,
+            organizer: int = False
+        ) -> None:
+        self.guest_id: str = guest_id
         self.first_name = first_name
         self.last_name = last_name
         self.birth = birth
-        self.sex = sex
+        # self.sex = sex
 
         self.balance = balance # As centimes, 
-        self.adherant_bde = adherant_bde
+        # self.adherant_bde = adherant_bde
+        self.adherant_bde: bool
         # self.alchool_level: float # g/100ml (average human have 5L of blood)n TODO : Rename this.
-    
+        self.staff: bool = staff # Pour les staffs
+        self.organizer: bool = organizer # Pour les organisateurs
+
     def is_adult(self):
         # TODO
         today = datetime.datetime.today()
@@ -32,6 +46,9 @@ class Guest():
                 if self.birth.day <= today.day:
                     return True
         return False
+
+    def display_name(self):
+        return self.first_name + " " + self.last_name
 
 
 # user = Guest(
