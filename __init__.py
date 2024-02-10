fc59de15977761fcb5ce5f1a06de0feb34b766fe
Aodren COMMUN <69194856+Deden-77@users.noreply.github.com>
2024-02-10 17:19:20 +0100

    Add HTTP server launch (option 2) and add LoRa name, does nothing yet (option 3)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # https://patorjk.com/software/taag/#p=display&f=Thin&t=made%20by%20_Deden
 # https://patorjk.com/software/taag/#p=display&f=Merlin1&t=%20Smart%0A%20%20%20%20%20%20%20%20Event%0ABracelets
-
+import server.seb_web_server as seb_web_server
 
 with open("assets/smart_event_bracelets.txt", "r") as file:
     print(file.read())
@@ -15,13 +15,16 @@ print("##                                                ##")
 print("##              Quel mode utiliser ?              ##")
 print("##                                                ##")
 print("##       (1) Client (Bar/Vestiaire/Entrée)        ##")
-print("##       (2) Serveur                              ##")
+print("##       (2) Serveur HTTP                         ##")
+print("##       (3) Pacerelle LoRa                       ##")
 print("##                                                ##")
 print("####################################################")
 print()
 mode = None
-# while not mode 
+# while not mode
 mode = input("      --> ")
 
 if mode == "1":
     import client.client
+if mode == "2":
+    seb_web_server.main()
